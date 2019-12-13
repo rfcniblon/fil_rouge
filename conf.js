ff04fc3c5c9e4f58af1a37fb48d7fa8873a8012b
rfcniblon <51241155+rfcniblon@users.noreply.github.com>
2019-12-13 13:21:42 +0100

    Update conf.js

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ const  mysql = require('mysql');
 const  connection = mysql.createConnection({
 host :  'localhost', // adresse du serveur
 user :  'root', // le nom d'utilisateur
-password :  'Ank@ed01', // le mot de passe
+password :  'houlalalala', // le mot de passe
 database :  'fil_rouge', // le nom de la base de données
 });
 module.exports = connection;
